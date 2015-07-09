ec1111bd29e4c0a71f88d3dce173cf49417c735f
Martin Minchorov <PBaller@Martins-MacBook-Pro.local>
2015-07-09 05:19:57 +0200

    Added Update and Delete thus completing Users CRUD. Also did some refactoring by adding find by userid middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,22 +15,77 @@ var routes = function(User){
             User.find(function(err, users){
                 if(err)
                     res.status(500).send(err)
-                else
-                    res.json(users)
+
+                res.json(users)
             })
         })
 
-    userRouter.route('/:username')
+    userRouter.route('/u/:username')
         .get(function(req, res){
 
-            User.find({ username: req.params.username }, function(err, username){
-                if(err) {
+            User.findOne({ username: req.params.username }, function(err, username){
+                if(err)
                     res.status(500).send(err);
-                }else {
-                    res.json(username);
-                }
+
+                res.json(username);
+
             });
         });
+
+
+    userRouter.use('/:userId', function(req, res, next){
+            User.findById(req.params.userId, function(err, user){
+                if(err)
+                    res.status(500).send(err);
+                else if(user) {
+                    req.user = user;
+                    next();
+                }else
+                    res.status(404).send('User Doesn\'t Exist');
+            })
+    })
+    userRouter.route('/:userId')
+        .get(function(req, res){
+            res.json(req.user);
+        })
+        .put(function(req, res){
+
+            req.user.username = req.body.username;
+            req.user.firstname = req.body.firstname;
+            req.user.lastname = req.body.lastname;
+            req.user.email = req.body.email;
+            req.user.role = req.body.role;
+            req.user.active = req.body.active;
+            req.user.subscription = req.body.subscription;
+            req.user.portrait = req.body.portrait;
+            req.user.save(function(err){
+                if (err)
+                    res.status(500).send(err);
+
+                res.json(req.user);
+            });
+
+        })
+        .patch(function(req, res){
+            if(req.body._id)
+                delete req.body._id;
+            for(u in req.body){
+                req.user[u] = req.body[u];
+            }
+            req.user.save(function(err){
+                if (err)
+                    res.status(500).send(err);
+
+                res.json(req.user);
+            });
+        })
+        .delete(function(req, res){
+            req.user.remove(function(err){
+                if(err)
+                    res.status(500).send(err);
+                res.status(204).send('User Successfully Removed!');
+            });
+        })
     return userRouter;
 };
 
